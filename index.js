6ed50122bb8c64400588e6a46837e8264041cfb6
Rosana <t_rmsouza@sfiec.org.br>
2023-07-15 15:25:49 -0300

    Coloca no repositorio

---
+++ index.js
@@ -0,0 +1,10 @@
+const day = document.getElementById('day')
+const night = document.getElementById('night')
+
+day.addEventListener("click", function(){
+    document.body.style.backgroundImage = 'url("https://i.pinimg.com/564x/35/d3/5b/35d35bab50560703577a10f27f2f6785.jpg")'
+})
+
+night.addEventListener("click", function(){
+    document.body.style.backgroundImage = 'url("https://i.pinimg.com/564x/d8/37/ef/d837ef3b34a12a8d55ab8e64e4c38a04.jpg")'
+})
